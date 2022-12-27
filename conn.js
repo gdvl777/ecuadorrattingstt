51bd4adbc5e1f5a9e3a5232154a697c00bea9987
gdvl777 <gdvl777@gmail.com>
2022-12-26 23:12:30 -0500

    Update conn.js

--- conn.js
+++ conn.js
@@ -1,1 +1,17 @@
 const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://localhost:27017/miBaseDeDatos', (err, db) => {
+  if (err) {
+    console.log('Error al conectar a la base de datos');
+  } else {
+    console.log('Conexión establecida con la base de datos');
+  }
+});
+
+
+
+
+
+
+
+
+db.close();
